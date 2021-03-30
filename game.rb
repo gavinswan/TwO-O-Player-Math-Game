b97a22457242434b9991db8455d9a7644f028891
gavinswan <gavtswan@gmail.com>
2021-03-30 01:43:25 +0000

    add switch_players method

--- game.rb
+++ game.rb
@@ -21,6 +21,18 @@ class Game
     print "> "
     @guess = $stdin.gets.chomp
   end
+
+  # switch which player is the current player
+  def switch_players
+    if @current_player.name == @player1.name
+      @current_player = @player2
+      @other_player = @player1
+    else 
+      @current_player = @player1
+      @other_player = @player2
+    end
+    return
+  end
 end
 
 gameOne = Game.new
